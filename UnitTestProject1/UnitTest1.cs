1113c0df9c45798374b94fe7c75f9d0a4ddcdf4e
mehrdadkamelzadeh <mehrdad.kamelzadeh@gmail.com>
2016-07-02 13:41:28 +0430

    AddUnitTest

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GitModisehTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var m=new Modi();
+            Assert.AreEqual(m.calc(),2);
+        }
+    }
+}
